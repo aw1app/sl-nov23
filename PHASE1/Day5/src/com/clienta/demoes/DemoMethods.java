f62f99bde25d05413ea1168ce3108d174729f3e8
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-11-27 17:30:48 +0530

    constructors

--- PHASE1/Day5/src/com/clienta/demoes/DemoMethods.java
+++ PHASE1/Day5/src/com/clienta/demoes/DemoMethods.java
@@ -7,10 +7,22 @@ public class DemoMethods {
 	public static void main(String[] args) {
 		A a1 = new A();
 		
+		System.out.println("a1.x="+a1.getValueOfx());
+		
 		a1.method1A();
 		
-		a1.method2A(3);
-
+		int result1 =a1.method2A(3);
+		System.out.println(result1);
+		
+		
+		int result2 = a1.method2A(3,5);
+		System.out.println(result2);
+		
+		
+		
+		/// Use the overloaded constructor
+		A a2 = new A(12,15);
+		System.out.println("a2.x="+a2.getValueOfx());
 	}
 	
 	
--- PHASE1/Day5/src/package1/A.java
+++ PHASE1/Day5/src/package1/A.java
@@ -2,12 +2,30 @@ package package1;
 
 public class A {
 
-	private int x = 10;
+	private int x;
 	int y;
 
 	protected int z = 10;
 
 	public int p = 100;
+	
+	//Default Constructor
+	public A() {		
+		this.x=100;
+		this.y=100;		
+	}
+	
+	public A(int x, int y) {		
+		this.x=x;
+		this.y=y;		
+	}
+	
+	public int getValueOfx() {
+
+		 return this.x;
+
+	}
+	
 
 	public void method1A() {
 
@@ -18,6 +36,13 @@ public class A {
 	public int method2A(int a) {
 
 		return a*a;
+
+	}
+	
+	// An overloaded method of above method
+	public int method2A(int a, int b) {
+
+		return a*b;
 
 	}
 
