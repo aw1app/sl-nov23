d5b2ea09a056cd6216bd5b723dea383ef84a4da3
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-11-30 15:36:59 +0530

    Exceptions

---
+++ PHASE1/Day5/src/com/clienta/exceptions/PatientReportNotFound.java
@@ -0,0 +1,12 @@
+package com.clienta.exceptions;
+
+public class PatientReportNotFound extends Exception{
+	
+	String message;
+	
+	public PatientReportNotFound(String message) {
+		super(message);		
+	}
+	
+
+}
