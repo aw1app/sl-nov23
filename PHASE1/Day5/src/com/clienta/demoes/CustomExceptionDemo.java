b938e7a3a13d0e4c1f54d56c7587099296a18a87
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-11-30 15:45:54 +0530

    Exceptions

--- PHASE1/Day5/src/com/clienta/demoes/CustomExceptionDemo.java
+++ PHASE1/Day5/src/com/clienta/demoes/CustomExceptionDemo.java
@@ -1,11 +1,31 @@
 package com.clienta.demoes;
 
+import java.util.Scanner;
+
+import com.clienta.exceptions.PatientReportNotFound;
+
 public class CustomExceptionDemo {
 
 	public static void main(String[] args) {
-		
-		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter the id of the pateint whose record is to be found  ");
+		int id = sc.nextInt();
+
+		System.out.println("Thank you.");
 
 	}
 
+}
+
+class PatientRecord {
+
+	static String findRecord(int id) {
+		// assume that patient records are available only for id from 1 to 10.
+		if (id >= 0 && id <= 10) {
+			return " Found the record. Will Email it";
+		} else {
+			throw new PatientReportNotFound("records with such id not found");
+		}
+	}
 }
