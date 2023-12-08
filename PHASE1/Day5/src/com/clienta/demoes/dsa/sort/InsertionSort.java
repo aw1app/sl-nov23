0922a37c30b6d1ac4741c1b5d78481d22b621dd7
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-08 14:58:41 +0530

    dsa sorting

---
+++ PHASE1/Day5/src/com/clienta/demoes/dsa/sort/InsertionSort.java
@@ -0,0 +1,40 @@
+package com.clienta.demoes.dsa.sort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int arr[] = { 10, 2, 77, 23, 21, 50, 3, 17 };
+
+		System.out.println("Given array: " + Arrays.toString(arr));
+
+		insertionSort(arr);
+
+		System.out.println("Sorted array: " + Arrays.toString(arr));
+
+
+	}
+
+	private static void insertionSort(int[] arr) {
+		
+		for(int i=1; i< arr.length; i++) {
+			
+			int temp = arr[i];
+			
+			int j = i-1;
+			
+			while( j>-1 && temp<arr[j]) {
+				//swaping
+				arr[j + 1] = arr[j];
+				arr[j] =temp;
+				
+				j--;				
+			}
+			
+		}
+		
+		
+	}
+
+}
