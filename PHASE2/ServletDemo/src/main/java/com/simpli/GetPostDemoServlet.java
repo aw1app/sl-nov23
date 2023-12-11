c41ec3673510958dc34a07b14cbee3213104e61a
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-11 15:26:25 +0530

    Servlets G and P diff

--- PHASE2/ServletDemo/src/main/java/com/simpli/GetPostDemoServlet.java
+++ PHASE2/ServletDemo/src/main/java/com/simpli/GetPostDemoServlet.java
@@ -8,14 +8,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/gpdemo")
+@WebServlet(urlPatterns = {"/gpdemo", "/gp" , "/abc/*"})
 public class GetPostDemoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw = response.getWriter(); // CTRL +Shift + O
 
-		pw.write(" Recieved a GET request!");
+		pw.write(" Recieved a GET request!. "
+				+ "Notice that whatever data you submiited in the form feilds, WILL APPEAR in the browser's address bar!! ");
 
 		pw.close();
 	}
