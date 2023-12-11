6cb5c21c840214ecdd9f6f80202e6a468847fb58
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-11 15:43:21 +0530

    Servlet Forms

--- PHASE2/ServletDemo/src/main/java/com/simpli/UserLoginFormProcessingServlet.java
+++ PHASE2/ServletDemo/src/main/java/com/simpli/UserLoginFormProcessingServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/form-processing-servlet")
 public class UserLoginFormProcessingServlet extends HttpServlet {
@@ -14,7 +15,18 @@ public class UserLoginFormProcessingServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		PrintWriter pw = response.getWriter(); 
 		
+		String uname = request.getParameter("username");
+		String upwd = request.getParameter("passwd");
+		
+		String rememberme = request.getParameter("rememberMe");
+		
+		System.out.printf(" user submitted  username=%s passwd=%s rememberMe=%s \n", uname, upwd,rememberme );	
+		
+		pw.write("Hey "+ uname + ", thanks for logging in!");
+		
+		pw.close();
 
 	}
 
