46b09e5f9549ee55d4e796e5cebece3630e07f67
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-11 15:36:34 +0530

    Servlet Forms

---
+++ PHASE2/ServletDemo/src/main/java/com/simpli/UserLoginFormProcessingServlet.java
@@ -0,0 +1,21 @@
+package com.simpli;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/form-processing-servlet")
+public class UserLoginFormProcessingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		
+
+	}
+
+}
