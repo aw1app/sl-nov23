2da48b9aeb5ccc9be449df0ef22c55bbb081b1c2
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-14 15:18:55 +0530

    Database

--- PHASE2/JDBDemo/src/main/java/com/simpli/JDBCDemo.java
+++ PHASE2/JDBDemo/src/main/java/com/simpli/JDBCDemo.java
@@ -8,31 +8,52 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class JDBCDemo extends HttpServlet {
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		PrintWriter pw = response.getWriter();
-		
-		
-		// 1. Loading the JDBC Driver
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		//2.
-		Connection connection  = DriverManager.getConnection("localhost:3306","root","rootroot" );
-		
-		//3.
-		Statement stmt = connection.createStatement();
-		
-		stmt.executeQuery("CREATE DATABASE ecommerce");
-		
-		//4. Process the results
-		
-	}
 
-	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter pw = response.getWriter();
+
+		// 1. Loading the JDBC Driver
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Connection connection = null;
+
+		try {
+			// 2. Get the connection to the data base
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "rootroot");
+
+			// 3.
+			Statement stmt = connection.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT * FROM ORDERS");
+
+			// 4. Process the results
+			// Loop through all the rows of the query results
+			pw.write("QUERY RESULTS <hr>");
+			while (rs.next()) {
+
+				int orderId = rs.getInt("ORDER_ID");
+				float amount = rs.getFloat("AMOUNT");
+				String customerName = rs.getString("CUSTOMER_NAME");
+
+				pw.printf(" Order ID=%s, amount=%s customerName=%s <br> ", orderId, amount, customerName);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
