450ddbeccecf232562e1e12e4429eedd0e8d5734
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-26 17:39:13 +0530

    update

---
+++ PHASE3/ecommercehibernate/src/main/java/com/ecommerce/dao/EProductDAO.java
@@ -0,0 +1,25 @@
+package com.ecommerce.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.entity.EProductEntity;
+
+@Repository
+public class EProductDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	// Business Methods
+
+	public List<EProductEntity> getAllProducts() {
+		return this.sessionFactory.getCurrentSession().createQuery("from EProductEntity").list();
+	}
+
+}
+
+
