dd8ca52d8511a59c619a7581054bb4a46ca32319
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-27 17:51:27 +0530

    update

--- PHASE3/spring-boot-jpa-hibernate-demo/src/main/java/com/ecommerce/SpringBootJpaHibernateDemoApplication.java
+++ PHASE3/spring-boot-jpa-hibernate-demo/src/main/java/com/ecommerce/SpringBootJpaHibernateDemoApplication.java
@@ -2,7 +2,11 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
+@ComponentScan({"com.ecommerce.controllers", "com.ecommerce.entity","com.ecommerce.repositry" })
 @SpringBootApplication
 public class SpringBootJpaHibernateDemoApplication {
 
--- PHASE3/spring-boot-jpa-hibernate-demo/src/main/java/com/ecommerce/entity/EProduct.java
+++ PHASE3/spring-boot-jpa-hibernate-demo/src/main/java/com/ecommerce/entity/EProduct.java
@@ -3,12 +3,7 @@ package com.ecommerce.entity;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="eproduct")
