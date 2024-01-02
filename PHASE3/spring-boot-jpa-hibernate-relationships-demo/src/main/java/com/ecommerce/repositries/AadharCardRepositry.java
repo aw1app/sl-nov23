b3d273cd16a95b639fe074f27a15baf8ead17bbc
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-02 16:26:19 +0530

    update

---
+++ PHASE3/spring-boot-jpa-hibernate-relationships-demo/src/main/java/com/ecommerce/repositries/AadharCardRepositry.java
@@ -0,0 +1,9 @@
+package com.ecommerce.repositries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.entity.AadharCard;
+
+public interface AadharCardRepositry extends JpaRepository<AadharCard, Long>{
+
+}
--- PHASE3/spring-boot-jpa-hibernate-relationships-demo/src/main/java/com/ecommerce/repositries/UserRepositry.java
+++ PHASE3/spring-boot-jpa-hibernate-relationships-demo/src/main/java/com/ecommerce/repositries/UserRepositry.java
@@ -1,3 +1,5 @@
+package com.ecommerce.repositries;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecommerce.entity.User;
