d647ca744c9c76db02c6ad5453d651653fd7b57c
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-02 16:20:29 +0530

    update

--- PHASE3/spring-boot-jpa-hibernate-relationships-demo/src/main/java/com/ecommerce/entity/User.java
+++ PHASE3/spring-boot-jpa-hibernate-relationships-demo/src/main/java/com/ecommerce/entity/User.java
@@ -18,4 +18,23 @@ public class User {
 	
 	private String name;
 
+	public long getID() {
+		return ID;
+	}
+
+	public void setID(long iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
 }
