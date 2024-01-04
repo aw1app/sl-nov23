7dccc97d2cac7c32b072153e290b82e4258ded5e
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-04 17:47:56 +0530

    update

--- PHASE3/spring-boot-building-rest-api-server/src/main/java/com/ecommerce/controller/ProductController.java
+++ PHASE3/spring-boot-building-rest-api-server/src/main/java/com/ecommerce/controller/ProductController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,5 +44,15 @@ public class ProductController {
 		List<EProduct> products = eProductRepositry.findAll();
 		return products;
 	}
+	
+	
+	@PostMapping("/add-product")
+	public EProduct addProduct(@RequestBody EProduct eProduct) {
+		
+		EProduct savedProduct = eProductRepositry.save(eProduct);
+		return savedProduct;
+		
+	}
+	
 
 }
