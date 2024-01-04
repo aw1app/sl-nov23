3fd605411958dadcded1793385e0d18af62ea319
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-04 17:17:08 +0530

    update

--- PHASE3/spring-boot-building-rest-api-server/src/main/java/com/ecommerce/controller/ProductController.java
+++ PHASE3/spring-boot-building-rest-api-server/src/main/java/com/ecommerce/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.ecommerce.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController {
+	
+	
+	
+	
 
 }
