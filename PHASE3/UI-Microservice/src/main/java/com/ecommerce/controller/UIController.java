f9d6e6e39d674cac0d5a33dae54e3ad14392d33c
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-05 17:00:34 +0530

    update

--- PHASE3/UI-Microservice/src/main/java/com/ecommerce/controller/UIController.java
+++ PHASE3/UI-Microservice/src/main/java/com/ecommerce/controller/UIController.java
@@ -1,10 +1,14 @@
 package com.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UIController {
+	
+	@Autowired
+	RestTemplate restTemplate;
 
 
 }
