44a50d2b792f8219b4431e14880ad33a56cf1ec8
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-09 15:20:55 +0530

    update

--- PHASE3/JUnitDemo/src/com/simpli/CalculatorTest.java
+++ PHASE3/JUnitDemo/src/com/simpli/CalculatorTest.java
@@ -1,38 +1,74 @@
 package com.simpli;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("My good calaculator test")
 class CalculatorTest {
+	static Calculator cal1;
+
+	@BeforeAll
+	public static void testAddDoSomeThingBeforeExecutingTheFirstTestCase() {
+		System.out.println(" INSIDE BEFORE ALL testAddDoSomeThingBeforeExecutingTheFirstTestCase method");
+		cal1 = new Calculator();
+	}
+
+	@BeforeEach
+	public void testAddDoSomeThingBeforeExecutingTestcase() {
+		System.out.println(" INSIDE BEFORE EACH testAddDoSomeThingBeforeExecutingTestcase method");
+	}
 
 	@Test
+	@Tag("addition")
 	public void testAddWhenZeroZero() {
-		
-		Calculator cal1 = new Calculator();
-		
-		int a=0;
-		int b=0;
-		int expectedvalue =0;
-		
-		int result = cal1.add(a,b);	
-		
-		
-		assertTrue(result==expectedvalue);		
+
+		int a = 0;
+		int b = 0;
+		int expectedvalue = 0;
+
+		int result = cal1.add(a, b);
+
+		assertTrue(result == expectedvalue);
 	}
-	
+
 	@Test
 	public void testAddWhenPositiveNegative() {
-		
-		Calculator cal1 = new Calculator();
-		
-		int a=10;
-		int b=-6;
-		int expectedvalue =4;
-		
-		int result = cal1.add(a,b);		
-		
-		assertTrue(result==expectedvalue);		
+
+		int a = 10;
+		int b = -6;
+		int expectedvalue = 4;
+
+		int result = cal1.add(a, b);
+
+		assertEquals(result, expectedvalue);
+	}
+
+	@Test
+	public void testDivisionWhenPositiveZero() {
+
+		int a = 10;
+		int b = 0;
+
+		assertThrows(ArithmeticException.class, () -> cal1.divide(a, b));
+	}
+
+	@AfterEach
+	public void testAddDoSomeThingAfterExecutingTestcase() {
+		System.out.println(" INSIDE AFTER EACH testAddDoSomeThingAfterExecutingTestcase method");
+	}
+
+	@AfterAll
+	public static void testAddDoSomeThingAfterExecutingTheLastTestCase() {
+		System.out.println(" INSIDE AFTER ALL testAddDoSomeThingAfterExecutingTheLastTestCase method");
 	}
 
 }
