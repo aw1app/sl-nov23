d2d51a2971625be1efa59e158ec932020a5ce40d
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-09 15:57:58 +0530

    update

--- PHASE3/JUnitDemo/src/com/simpli/DynamicTestsDemo.java
+++ PHASE3/JUnitDemo/src/com/simpli/DynamicTestsDemo.java
@@ -2,6 +2,7 @@ package com.simpli;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -10,17 +11,31 @@ import org.junit.jupiter.api.TestFactory;
 
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
-
 class DynamicTestsDemo {
 
 	@TestFactory
 	Collection<DynamicTest> dynamicTests1() {
 
-		DynamicTest dynTest1 = dynamicTest("Dynamic test 1", () -> assertTrue(7 ==new Calculator().add(2, 5)));
+		DynamicTest dynTest1 = dynamicTest("Dynamic test 1", () -> assertTrue(7 == new Calculator().add(2, 5)));
+
+		DynamicTest dynTest2 = dynamicTest("Dynamic test 2 for cal div",
+				() -> assertTrue(2 == new Calculator().divide(5, 2)));
+
+		return Arrays.asList(dynTest1, dynTest2);
+	}
+
+	@TestFactory
+	Collection<DynamicTest> dynamicTests2() {
+
+		Collection<DynamicTest> dynamicTests = new ArrayList<DynamicTest>();
+
 		
-		DynamicTest dynTest2 = dynamicTest("Dynamic test 2 for cal div", () -> assertTrue(2 ==new Calculator().divide(5, 2)));
-		
-		return Arrays.asList(dynTest1,dynTest2);
+		for (int i = 0; i < 5; i++) {
+			int x=i;
+			dynamicTests.add(dynamicTest("Dynamic test for a="+x, () -> assertTrue(7 == new Calculator().add(x, 5))));
+		}
+
+		return dynamicTests;
 
 	}
 
