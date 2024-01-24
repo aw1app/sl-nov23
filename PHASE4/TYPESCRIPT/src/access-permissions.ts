286e6c122c37e2d0eb0a7e4df3f114203f320b04
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-24 10:15:54 +0530

    update

--- PHASE4/TYPESCRIPT/src/access-permissions.ts
+++ PHASE4/TYPESCRIPT/src/access-permissions.ts
@@ -1,22 +1,22 @@
 
-class Bus{
-    name:string;
-    private mileage:number;
-    protected age:number;
+class Bus {
+    name: string;
+    private mileage: number;
+    protected age: number;
 
     readonly engineType: string;
 
-    constructor(name:string, mileage:number , age:number, engineType: string){
-        this.name=name;
-        this.mileage=mileage;
-        this.age=age;
-        this.engineType= engineType;
+    constructor(name: string, mileage: number, age: number, engineType: string) {
+        this.name = name;
+        this.mileage = mileage;
+        this.age = age;
+        this.engineType = engineType;
     }
 
-    showMileage = ():number => this.mileage; 
+    showMileage = (): number => this.mileage;
 }
 
-let bus3 : Bus = new Bus("ford",100, 90, "petrol"); // in TS
+let bus3: Bus = new Bus("ford", 100, 90, "petrol"); // in TS
 
 //console.log(`Mileage is ${bus3.mileage}`); // ERROR mileage is private
 console.log(`Mileage is ${bus3.showMileage()}`);
