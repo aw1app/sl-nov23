410524359a4df1bd57430c41a6fef5cd706d2a74
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-29 17:51:51 +0530

    update

--- PHASE4/ANGULAR/my-angular-project/src/app/app.component.ts
+++ PHASE4/ANGULAR/my-angular-project/src/app/app.component.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { ProductV1Component } from './product-v1/product-v1.component';
+import { ProductlistV1Component } from './productlist-v1/productlist-v1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProductComponent, ProductV1Component],
+  imports: [CommonModule, RouterOutlet, 
+    ProductComponent,
+    ProductV1Component,
+    ProductlistV1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- PHASE4/ANGULAR/my-angular-project/src/app/productlist-v1/productlist-v1.component.ts
+++ PHASE4/ANGULAR/my-angular-project/src/app/productlist-v1/productlist-v1.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-productlist-v1',
+  selector: 'productlist-v1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './productlist-v1.component.html',
   styleUrl: './productlist-v1.component.css'
 })
@@ -14,8 +15,9 @@ export class ProductlistV1Component {
 
   constructor(){
     this.products[0] = new Product(1,"HP Mouse","../../assets/images/HPMouse.JPG",250);
-    this.products[1] = new Product(2,"iPhone","../../assets/images/Apple-Phone.JPG.JPG",50250);
+    this.products[1] = new Product(2,"iPhone","../../assets/images/Apple-Phone.JPG",50250);
     this.products[2] = new Product(3,"Dell Monitor","../../assets/images/dell.JPG",15250);
+    this.products[3] = new Product(4,"Lenova Keyboard","https://5.imimg.com/data5/IA/UN/MY-3208689/lenovo-laptops-.png", 2500);
   }
 
 }
