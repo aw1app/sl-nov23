0028b9dcda040315560e59134a73d0ad2785c7f1
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-29 17:12:28 +0530

    update

--- PHASE4/ANGULAR/my-angular-project/src/app/product-v1/product-v1.component.ts
+++ PHASE4/ANGULAR/my-angular-project/src/app/product-v1/product-v1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../model/product';
 
 @Component({
@@ -8,12 +8,24 @@ import { Product } from '../model/product';
   templateUrl: './product-v1.component.html',
   styleUrl: './product-v1.component.css'
 })
-export class ProductV1Component {
+export class ProductV1Component implements OnInit {
 
   product!: Product;
+
+  // Let's recieve the following inputs from the parent component
+  @Input("name") name!:string
+  @Input("image") image!:string
+  @Input("price") price!:string
   
-  constructor(){
-    this.product = new Product(1,"HP Mouse","../../assets/images/HPMouse.JPG",250);
+  // constructor(){
+  //   this.product = new Product(1,"HP Mouse","../../assets/images/HPMouse.JPG",250);
+  // }
+
+  constructor(){    
+  }
+
+  ngOnInit(): void {
+    this.product = new Product(1,this.name,this.image,Number(this.price));
   }
 
 }
