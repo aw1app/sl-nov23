249d172712c7bf43de05a2eca4551140840b2f3d
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-01 17:55:19 +0530

    update

--- PHASE4/ANGULAR/project-using-service/src/app/addproduct/addproduct.component.ts
+++ PHASE4/ANGULAR/project-using-service/src/app/addproduct/addproduct.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductService } from '../product.service';
+import { Productv1Service } from '../productv1.service';
 
 @Component({
   selector: 'addproduct',
@@ -14,22 +15,31 @@ export class AddproductComponent {
 
   productForm!: FormGroup;
 
-  productService!: ProductService;
+  // productService!: ProductService;
+  productService!: Productv1Service;
 
-  constructor(productService: ProductService, private fb: FormBuilder){
+  constructor(productService: Productv1Service, private fb: FormBuilder) {
 
     this.productService = productService;
-    
+
     this.productForm = this.fb.group(
       {
         productName: ['', [Validators.required, Validators.minLength(3)]],
         productPrice: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/), Validators.max(100000)]]
       }
-    ); 
+    );
 
   }
 
-  addProduct= ():number =>  this.productService.addProduct(this.productForm.value.productName,this.productForm.value.productPrice);
-  
+  //addProduct= ():number =>  this.productService.addProduct(this.productForm.value.productName,this.productForm.value.productPrice);
+
+  addProduct = (): void => {
+    
+    this.productService.addProduct(this.productForm.value.productName, this.productForm.value.productPrice)
+    .subscribe();
+
+
+  }
+
 
 }
--- PHASE4/ANGULAR/project-using-service/src/app/productv1.service.ts
+++ PHASE4/ANGULAR/project-using-service/src/app/productv1.service.ts
@@ -8,8 +8,8 @@ import { Observable, throwError } from 'rxjs';
 })
 export class Productv1Service {
 
-  //SpringBoot project host URL
-  backEndUrl: String = "http://localhost:3000/products"; 
+  //JSON Server host URL
+  backEndUrl: String = "http://localhost:3000/products";
 
   products: Product[] = [];
 
@@ -20,17 +20,29 @@ export class Productv1Service {
   }
 
   //SpringBoot integration
-  getAllProductsFromSpringBootBackend(): Observable<Product[]> 
-  {
+  getAllProductsFromSpringBootBackend(): Observable<Product[]> {
     return this.httpClient.get<Product[]>(`${this.backEndUrl}`);
   }
 
-  getProductFromSpringBootBackend(id:number): Observable<Product> 
-  {
+  getProductFromSpringBootBackend(id: number): Observable<Product> {
     return this.httpClient.get<Product>(`${this.backEndUrl}/${id}`);
   }
 
-  deleteProduct = (id: number): Observable<string> => {   
+  newIdsStart:number=10000;
+
+  addProduct(name: string, price: number): Observable<any>  {
+
+    let id: number = this.newIdsStart++;   
+
+    let prod: Product = new Product(id, name," ", price, true);
+
+    const headers = { 'content-type': 'application/json' };
+    const prodJSON = JSON.stringify(prod);
+
+    return this.httpClient.post(`${this.backEndUrl}`, prodJSON, { 'headers': headers });
+  }
+
+  deleteProduct = (id: number): Observable<string> => {
     return this.httpClient.get<string>(`${this.backEndUrl}/delete-product/${id}`);
   };
 
