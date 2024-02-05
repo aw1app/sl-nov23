c28c302065ab1aefdb88f8bdc813574499299945
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-05 15:56:46 +0530

    update

--- PHASE5/hello-selenium/src/main/java/com/ecommerce/HelloWorldSelenium.java
+++ PHASE5/hello-selenium/src/main/java/com/ecommerce/HelloWorldSelenium.java
@@ -1,21 +1,23 @@
 package com.ecommerce;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelloWorldSelenium {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		// Basic configuration
-		WebDriver driver = new ChromeDriver();
-		
-		String baseUrl = "https://www.google.com";
-		
+		WebDriver driver = new FirefoxDriver();
+
+		String baseUrl = "https://chromewebstore.google.com/detail/selenium-ide/mooikfkahbdckldjjndioackbalphokd";
+
 		driver.get(baseUrl);
-		
-		System.out.println("Title of webpage "+driver.getTitle());
-		
+
+		System.out.println("Title of webpage " + driver.getTitle());
+
+		Thread.sleep(1000);
+
 		driver.close();
 
 	}
