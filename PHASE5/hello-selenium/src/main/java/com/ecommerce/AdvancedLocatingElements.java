90d46b043dfc3b945865278d12124cf02e40ce34
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-06 15:36:22 +0530

    update

--- PHASE5/hello-selenium/src/main/java/com/ecommerce/AdvancedLocatingElements.java
+++ PHASE5/hello-selenium/src/main/java/com/ecommerce/AdvancedLocatingElements.java
@@ -12,26 +12,36 @@ public class AdvancedLocatingElements {
 	static WebDriver driver = new ChromeDriver();
 
 	public static void main(String[] args) {
-		
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+
 		demoAdvancedXPathCSSSelector();
-		
 
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.close();
 	}
-	
-	
-	static void demoAdvancedXPathCSSSelector(){
+
+	static void demoAdvancedXPathCSSSelector() {
 		String baseUrl = "file:///F:/Users/home/git/sl-nov23/PHASE5/hello-selenium/src/main/resources/test.html";
-		
+
 		driver.get(baseUrl);
-		
+
 		List<WebElement> inputTagElements = driver.findElements(By.xpath("//input[contains(@id, 'admin')]"));
+
+		System.out.println(" Size of inputTagElements = " + inputTagElements.size()); // ?? 3
+
+		// Using CSS
+		List<WebElement> inputTagElementsUsingCSS = driver.findElements(By.cssSelector("input[id*='admin']"));
+
+		System.out.println(" Size of inputTagElementsUsingCSS = " + inputTagElementsUsingCSS.size()); // ?? 3
+
+		List<WebElement> inputXYZInIDElementsUsingCSSSelectors2 = driver.findElements(By.cssSelector("input[id$='xyz']"));
+		System.out.println("input  whose ids end with xyz " + inputXYZInIDElementsUsingCSSSelectors2.size()); // 2
 		
-		System.out.println(" Size of inputTagElements = "+inputTagElements.size()); // ?? 3		
 		
+		WebElement h4SecondChildInsideDiv = driver.findElement(By.cssSelector("div[id='eCommerce'] h4:nth-child(2)"));
+		System.out.println("h4SecondChildInsideDiv text is " + h4SecondChildInsideDiv.getText()); // h4 second
+
 	}
 
 }
