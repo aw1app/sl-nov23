c8a2175bb3250b30ea65bfd6618505f2b5ce0738
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-09 15:22:41 +0530

    update

--- PHASE5/hello-selenium-testng/src/main/java/com/ecommerce/test/GridTest.java
+++ PHASE5/hello-selenium-testng/src/main/java/com/ecommerce/test/GridTest.java
@@ -3,15 +3,21 @@ package com.ecommerce.test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class GridTest {
 	public WebDriver driver;
+	SoftAssert softAssert = new SoftAssert();
 	
 	@BeforeSuite
 	public void beforeSuite() {
@@ -31,6 +37,58 @@ public class GridTest {
 	@AfterSuite
 	public void afterSuite() {		
 		driver.quit();
+	}
+	
+	
+	@Test(groups={"Group1"})
+	public void searchGoogleAndTestTitleText() {
+		
+		System.out.printf("Inside %s and thread-id is %s \n", "searchGoogleAndTestTitleText", Thread.currentThread().getId());
+
+		driver.get("http://www.google.com");
+
+		System.out.println("Title of google page is " + driver.getTitle());
+
+		softAssert.assertEquals(driver.getTitle(), "Google");
+
+		softAssert.assertAll("Google title did not match");
+	}
+	
+	
+	@Test(enabled=false, dataProvider = "googleNewUserData")
+	public void googleNewAccCreation(String firstName, String lastName) throws InterruptedException {
+
+		String baseUrl = "https://accounts.google.com/signup/v2/createaccount?flowEntry=SignUp";
+		driver.get(baseUrl);
+
+		// Locate the first name feild using id
+		WebElement firstNameTF = driver.findElement(By.id("firstName"));
+		
+		softAssert.assertNotNull(firstNameTF);
+		firstNameTF.sendKeys(firstName);
+
+		// Locate the last name feild using name
+		WebElement lastNameTF = driver.findElement(By.name("lastName"));
+		
+		softAssert.assertNotNull(lastNameTF);		
+		lastNameTF.sendKeys(lastName);
+		
+		Thread.sleep(10000);
+		
+	}
+	
+	@DataProvider(name = "googleNewUserData")
+	public Object[][] googleNewUserData() {
+		return new Object[][] {
+
+				{ "fname1", "lname1" },
+				{ "fname2__2", "lname2__2" }
+			};
+	}
+	
+	@Test(groups={"Group2"})
+	public void f3() {
+		System.out.printf("Inside %s and thread-id is %s \n", "f3 in GoogleSearch class in Group2", Thread.currentThread().getId());
 	}
 
 }
